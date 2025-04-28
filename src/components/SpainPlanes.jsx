6e302e47b29548d0cbf84e545b9c385cd46a1bc8
Alex Abad <01abadalejandro@gmail.com>
2025-04-28 08:57:38 +0200

    lloro

--- src/components/SpainPlanes.jsx
+++ src/components/SpainPlanes.jsx
@@ -7,7 +7,7 @@ function SpainPlanes() {
 
   const fetchData = () => {
     setUpdating(true);
-    fetch('/api/SpainPlanes')
+    fetch('/api/SpainPlane')
       .then(response => {
         if (!response.ok) {
           throw new Error('Error en la solicitud: ' + response.statusText);
