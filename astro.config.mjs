0384d733f2894222bb5fa96a0a3373caa7920081
Alex Abad <01abadalejandro@gmail.com>
2025-05-06 09:58:30 +0200

    A

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import setupProxy from './proxy.js';
+import path from 'path';
 
 export default defineConfig({
   integrations: [react()],
   server: {
     middleware: [setupProxy],
+  },
+  alias: {
+    '@components': path.resolve('./src/components'),
   },
 });
