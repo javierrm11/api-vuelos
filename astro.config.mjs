a002b96dd3998c913cdf797482eb0e5927745509
Javier <javierrumo2@gmail.com>
2025-05-06 11:08:00 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
- import react from '@astrojs/react';
- import setupProxy from './proxy.js';
- import path from 'path';
- 
- export default defineConfig({
-   integrations: [react()],
-   server: {
-     middleware: [setupProxy],
-   },
-   alias: {
-     '@components': path.resolve('./src/components'),
-   },
- });
+import react from '@astrojs/react';
+import setupProxy from './proxy.js';
+
+export default defineConfig({
+  integrations: [react()],
+  server: {
+    middleware: [setupProxy],
+  },
+});
