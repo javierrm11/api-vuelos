f2a181707bb086354589b47a3407c9a0e61c93ba
Javier <javierrumo2@gmail.com>
2025-06-08 12:44:09 +0200

    fixs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
     return (
-      <footer className="lg:pl-64 bottom-0 bg-secondary text-white text-center py-3 z-50 shadow-md">
-        <p className="text-sm">© 2025 ApiVuelos</p>
+      <footer className="lg:pl-64 bottom-0 bg-secondary dark:bg-border text-text text-center py-3 z-50 shadow-md">
+        <p className="text-sm">
+          @ {new Date().getFullYear()} ApiVuelos - Todos los derechos reservados.
+        </p>
       </footer>
     );
   }
